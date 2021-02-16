93c27fd96bbee0ec76263025cc8eef64388c2ea2
神田舞 <maiptomato@gmail.com>
2021-02-16 13:36:57 +0900

    データの送信

--- app/src/app/heroes/heroes.component.ts
+++ app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock/mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,14 +14,30 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Mai'
   };
-  heroes = HEROES;
+  heroes: Hero[] = [
+    {id: 1, name: ""},
+    {id: 1, name: ""},
+  ];
   selectedHero?: Hero;
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  constructor() { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
