c305040145a76a20e056d07f0900b825b6a9fb96
神田舞 <maiptomato@gmail.com>
2021-02-16 11:29:03 +0900

    clickイベント

---
+++ app/src/app/mock/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'maimai'},
+    {id: 12, name: 'mai'},
+    {id: 13, name: 'aiai'},
+    {id: 14, name: 'mamai'},
+]
